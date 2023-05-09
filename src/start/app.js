b4c1e4229b9cb5789d466d178b70ba27f665baeb
mhadi.kazemian@gmail.com <mhadi.kazemian@gmail.com>
2023-05-09 22:40:27 +0330

    Add helmet wrapper (httpSecurityMiddleware) to app.js for improved security.

--- src/start/app.js
+++ src/start/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const httpSecurityMiddleware = require('./middleware/http-security-middleware');
 
 const app = express();
+
+app.use(httpSecurityMiddleware);
 
 app.use(express.json());
 
